585dd201add00d107161f805d73f41a71996aae4
AgostonAttila <atesz014@gmail.com>
2020-02-02 17:24:48 +0100

    Admin panel

--- DatingAppWithIdentity_/DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingAppWithIdentity_/DatingApp-SPA/src/app/_services/auth.service.ts
@@ -46,4 +46,15 @@ export class AuthService {
     const token = localStorage.getItem('token');
     return !this.jwtHelper.isTokenExpired(token);
   }
+
+  roleMatch(allowedRoles): boolean {
+    let isMatch = false;
+    const userRoles = this.decodedToken.role as Array<string>;
+    allowedRoles.array.forEach(element => {
+      if (userRoles.includes(element)) {
+        isMatch = true;
+      }
+    });
+    return isMatch;
+  }
 }
